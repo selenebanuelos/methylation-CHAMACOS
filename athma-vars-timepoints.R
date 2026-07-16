30022e1614bb991b3c2022b06e4faa5f40116adc
selenebanuelos <135679586+selenebanuelos@users.noreply.github.com>
2026-07-15 17:58:56 -0700

    Create table to display data

--- athma-vars-timepoints.R
+++ athma-vars-timepoints.R
@@ -17,32 +17,41 @@ asthma <- read.dta13('data-raw/de_la_Rosa_06.dta',
 
 # data wrangling ---------------------------------------------------------------
 # reformat data to use with gtsummary::tbl_summary()
-reformat <- asthma %>%
+long <- asthma %>%
   # make table with asthma related variables only
   select(newid, contains('asth_')) %>%
   # make data longer
   pivot_longer(cols = contains('asth_'),
                names_to = c('variable', 'age'),
-               names_pattern = 'asth_(.+)_(\\d+Y)$') %>%
-  # spread data wider
+               names_pattern = 'asth_(.+)_(\\d+Y)$')
+
+# spread data wider
+wide <- long %>%
   pivot_wider(id_cols = c(newid, age),
               names_from = variable,
               values_from = value)
 
 # factor time point variable to control order displayed in table
-reformat$age <- factor(reformat$age, 
-                       levels = c('5Y','7Y','9Y','10Y','12Y','14Y','16Y','18Y')
-                       )
+wide$age <- factor(wide$age,
+                   levels = c('5Y','7Y','9Y','10Y','12Y','14Y','16Y','18Y')
+                   )
 
 # data visualization -----------------------------------------------------------
 # table
-reformat %>%
+wide %>%
   tbl_summary(include = c(sym, symed, med, diag_ever), 
               by = age,
-              missing_text = 'Missing')
+              missing_text = 'Missing',
+              # show missing as n (%)
+              missing_stat = '{N_miss} ({p_miss}%)')
+# looks like there's different rates of missingness across vars at 9Y, 12Y, 14Y,
+# 16Y, 18Y
 
+# use plots below to visualize this info
 # spaghetti plots
-# plot counts over time
+# plot 'yes' responses over time for each of 3 vars
+long %>%
+  ggplot(aes(x = age, y = )
 
 
-# plot proportions over time 
+# plot 'missing' responses over time for each of 3 vars
